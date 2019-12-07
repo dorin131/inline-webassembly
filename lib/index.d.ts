0d46b60af0183ace0a87742613d486947c22c29a
Dorin <dorin.fodor@gmail.com>
2019-12-07 00:13:37 +0000

    feat(): Implemented readString method for reading from memory buffer

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,11 @@
-import { WasmModule } from "wabt";
+import { WasmModule as WM } from "wabt";
 
 declare namespace InlineWebAssembly {
 
+}
+
+interface WasmModule extends WM {
+  readString(index: number, length?: number): string
 }
 
 declare function InlineWebAssembly(wasm: string): Promise<WasmModule>;
