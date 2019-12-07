f197954eba21e4dcc45a5aaab63b588292f41f56
Dorin <dorin.fodor@gmail.com>
2019-12-07 15:56:24 +0000

    feat(): Added test

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,10 @@
 import { WasmModule as WM } from "wabt";
 
-declare namespace InlineWebAssembly {
-
-}
+declare namespace InlineWebAssembly {}
 
 interface WasmModule extends WM {
-  readString(index: number, length?: number): string
+  readString(index: number, length?: number): string;
+  createString(string: string, memoryLocation: number): number;
 }
 
 declare function InlineWebAssembly(wasm: string, importObject?: any): Promise<WasmModule>;
